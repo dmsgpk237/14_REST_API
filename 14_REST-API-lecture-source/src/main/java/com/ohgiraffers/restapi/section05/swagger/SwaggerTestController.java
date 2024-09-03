e7ed9e37d9d01cd438d51412adaf5c9e05b11eec
권은혜 <a01027947353@gmail.com>
2024-09-03 18:47:41 +0900

    굿바이

---
+++ 14_REST-API-lecture-source/src/main/java/com/ohgiraffers/restapi/section05/swagger/SwaggerTestController.java
@@ -0,0 +1,144 @@
+package com.ohgiraffers.restapi.section05.swagger;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.nio.charset.Charset;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+// Tag: API들의 그룹을 짓기 위한 어노테이션
+@Tag(name = "Spring boot swagger 연동 API(USER 기능)")
+@RestController
+@RequestMapping("/swagger")
+public class SwaggerTestController {
+
+    private List<UserDTO> users;
+
+    public SwaggerTestController() {
+        users = new ArrayList<>();
+
+        users.add(new UserDTO(1, "user01", "pass01", "너구리", LocalDate.now()));
+        users.add(new UserDTO(2, "user02", "pass02", "코알라", LocalDate.now()));
+        users.add(new UserDTO(3, "user03", "pass03", "깜지곰", LocalDate.now()));
+    }
+
+    // user 정보 전체 조회
+    @Operation(summary = "전체 회원 조회", description = "우리 사이트의 전체 회원 목록 조회")
+    @GetMapping("/users")
+    public ResponseEntity<ResponseMessage> findAllUsers() {
+
+        // Header
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(
+                new MediaType(
+                        "application",
+                        "json",
+                        Charset.forName("UTF-8")
+                )
+        );
+
+        // body
+        Map<String, Object> responseMap = new HashMap<>();
+        responseMap.put("users", users);
+
+        ResponseMessage responseMessage = new ResponseMessage(
+                200,
+                "조회 성공!",
+                responseMap
+        );
+
+        return new ResponseEntity<>(responseMessage, headers, HttpStatus.OK);
+    }
+
+    // 단일 조회
+    @Operation(
+            summary = "회원번호로 회원 조회",
+            description = "회원번호를 통해 회원을 조회한다.",
+            parameters = { @Parameter(name = "userNo", description = "사용자 화면에서 넘어오는 user의 pk")})
+    @GetMapping("/users/{userNo}")
+    public ResponseEntity<ResponseMessage> findUserByNo(@PathVariable int userNo){
+
+        // Header
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(
+                new MediaType(
+                        "application",
+                        "json",
+                        Charset.forName("UTF-8")
+                )
+        );
+
+        // body
+
+        UserDTO foundUser = users.stream().filter(user -> user.getNo() == userNo).toList().get(0);
+
+        Map<String, Object> responseMap = new HashMap<>();
+        responseMap.put("user", foundUser);
+
+        return ResponseEntity.ok().headers(headers).
+                body(new ResponseMessage(200, "조회 성공!", responseMap));
+
+    }
+
+    // 회원 추가
+    @PostMapping("/users")
+    public ResponseEntity<?> regist(@RequestBody UserDTO newUser){
+
+        System.out.println("newUser : " + newUser);
+
+        // 마지막에 위치한 유저 번호
+        int lastUserNo = users.get(users.size() - 1).getNo();
+
+        newUser.setNo(lastUserNo + 1);
+
+        // 유저 추가
+        users.add(newUser);
+
+        return ResponseEntity
+                // restfull
+                // 201 상태코드 -> 등록 관련(생성)
+                .created(URI.create("/entity/users/" + users.get(users.size() - 1).getNo())).build();
+    }
+
+    // 수정
+    @PutMapping("/users/{userNo}")
+    public ResponseEntity<?> modifyUser(@PathVariable int userNo, @RequestBody UserDTO modifyInfo) {
+
+        // 수정할 유저 정보 찾아오기
+        UserDTO foundUser = users.stream().filter(user -> user.getNo() == userNo).toList().get(0);
+        foundUser.setId(modifyInfo.getId());
+        foundUser.setId(modifyInfo.getId());
+        foundUser.setPwd(modifyInfo.getPwd());
+        foundUser.setName(modifyInfo.getName());
+
+        return ResponseEntity.created(URI.create("/entity/users/" + userNo)).build();
+    }
+
+    // 유저 삭제
+    @ApiResponses({
+            @ApiResponse(responseCode = "204", description = "회원정보 삭제 성공"),
+            @ApiResponse(responseCode = "400", description = "잘못 입력된 파라미터")
+    })
+    @DeleteMapping("/users/{userNo}")
+    public ResponseEntity<?> removeUser(@PathVariable int userNo){
+        UserDTO foundUser = users.stream().filter(user -> user.getNo() == userNo).toList().get(0);
+        users.remove(foundUser);
+
+        return ResponseEntity.noContent().build();
+    }
+
+
+}
